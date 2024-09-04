30be2f94346c289803737f70d428ce7dc3e483b2
Michael Aguirre Merchan <micamerchan@gmail.com>
2024-09-03 18:09:21 -0600

    first commit

---
+++ Unity/portal/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public Text text;
+    // Update is called once per frame
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.name == "GameOver")
+        {
+            text.text = "You Won!";
+        }
+    }
+}
